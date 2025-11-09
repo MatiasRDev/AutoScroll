c746fab5c350216ed4dda890957f940c7fb0f72f
Matías Ramírez Ojeda <172167336+MatiasRDev@users.noreply.github.com>
2025-11-09 00:41:53 -0300

    fix(userscript): ensure metadata header exposes bundle

--- autoscroll.user.js
+++ autoscroll.user.js
@@ -12,9 +12,6 @@
 // @supportURL    https://github.com/MatiasRDev/AutoScroll/issues
 // @updateURL     https://raw.githubusercontent.com/MatiasRDev/AutoScroll/main/autoscroll.user.js
 // @downloadURL   https://raw.githubusercontent.com/MatiasRDev/AutoScroll/main/autoscroll.user.js
-// @homepageURL  https://github.com/MatiasRDev/AutoScroll
-// @updateURL    https://raw.githubusercontent.com/MatiasRDev/AutoScroll/main/autoscroll.user.js
-// @downloadURL  https://raw.githubusercontent.com/MatiasRDev/AutoScroll/main/autoscroll.user.js
 // @icon         data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAxMjggMTI4Ij48cmVjdCBmaWxsPSIjMDA2ZDc3IiB3aWR0aD0iMTI4IiBoZWlnaHQ9IjEyOCIvPjxwYXRoIGZpbGw9IiNmZmZmZmYiIGQ9Ik02NCAyMGwyMiAyMkg3NHY0NGgxMkw2NCAxMDggNDIgODZoMTJWNDJINDJ6Ii8+PC9zdmc+
 // @require      https://raw.githubusercontent.com/MatiasRDev/AutoScroll/main/dist/autoscroll.bundle.js
 // @run-at       document-end
