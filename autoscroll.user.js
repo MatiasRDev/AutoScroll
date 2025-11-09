410ec0ac19ce593c6a80f362495120d79f1a9037
Matías Ramírez Ojeda <172167336+MatiasRDev@users.noreply.github.com>
2025-11-08 21:53:52 -0300

    fix: harden userscript metadata and ui safety

--- autoscroll.user.js
+++ autoscroll.user.js
@@ -3,10 +3,18 @@
 // @namespace    https://matias.ramirez/autoscroll
 // @version      1.0.0
 // @description  Auto-scroll configurable con panel avanzado
+// @author       Matías Ramírez
+// @license      MIT
 // @match        http*://*/*
+// @exclude      https://chrome.google.com/webstore/*
+// @exclude      https://addons.mozilla.org/*
+// @homepageURL  https://github.com/MatiasRDev/AutoScroll
 // @updateURL    https://raw.githubusercontent.com/MatiasRDev/AutoScroll/main/autoscroll.user.js
 // @downloadURL  https://raw.githubusercontent.com/MatiasRDev/AutoScroll/main/autoscroll.user.js
+// @icon         data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAxMjggMTI4Ij48cmVjdCBmaWxsPSIjMDA2ZDc3IiB3aWR0aD0iMTI4IiBoZWlnaHQ9IjEyOCIvPjxwYXRoIGZpbGw9IiNmZmZmZmYiIGQ9Ik02NCAyMGwyMiAyMkg3NHY0NGgxMkw2NCAxMDggNDIgODZoMTJWNDJINDJ6Ii8+PC9zdmc+
 // @require      https://raw.githubusercontent.com/MatiasRDev/AutoScroll/main/dist/autoscroll.bundle.js
+// @run-at       document-end
+// @noframes
 // @grant        GM_getValue
 // @grant        GM_setValue
 // @grant        GM_addStyle
