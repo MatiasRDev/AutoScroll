49db977d8bc784b8bb8688eb70acc9464441cd1c
Matías Ramírez Ojeda <172167336+MatiasRDev@users.noreply.github.com>
2025-11-23 01:44:34 -0300

    Fix Tampermonkey install link

--- README.md
+++ README.md
@@ -5,8 +5,7 @@ Autoscroll suave con panel configurable, gestos (clics / triple clic), pausa int
 ## Instalación
 
 1. Instala Tampermonkey (Chrome/Edge/Firefox).
-2. Click aquí para instalar el script:  
-   https://raw.githubusercontent.com/MatiasRDev/AutoScroll/main/autoscroll.user.js
+2. [Instalar en Tampermonkey](https://github.com/MatiasRDev/AutoScroll/raw/main/autoscroll.user.js)
 
 ## Atajos por defecto
 - Shift + A → Iniciar/Detener
--- autoscroll.user.js
+++ autoscroll.user.js
@@ -9,8 +9,8 @@
 // @exclude      https://chrome.google.com/webstore/*
 // @exclude      https://addons.mozilla.org/*
 // @homepageURL  https://github.com/MatiasRDev/AutoScroll
-// @updateURL    https://raw.githubusercontent.com/MatiasRDev/AutoScroll/main/autoscroll.user.js
-// @downloadURL  https://raw.githubusercontent.com/MatiasRDev/AutoScroll/main/autoscroll.user.js
+// @updateURL    https://github.com/MatiasRDev/AutoScroll/raw/main/autoscroll.user.js
+// @downloadURL  https://github.com/MatiasRDev/AutoScroll/raw/main/autoscroll.user.js
 // @icon         data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAxMjggMTI4Ij48cmVjdCBmaWxsPSIjMDA2ZDc3IiB3aWR0aD0iMTI4IiBoZWlnaHQ9IjEyOCIvPjxwYXRoIGZpbGw9IiNmZmZmZmYiIGQ9Ik02NCAyMGwyMiAyMkg3NHY0NGgxMkw2NCAxMDggNDIgODZoMTJWNDJINDJ6Ii8+PC9zdmc+
 // @require      https://raw.githubusercontent.com/MatiasRDev/AutoScroll/main/dist/autoscroll.bundle.js
 // @run-at       document-end
